dd0083106154b8975849baf377956dfe78bd5306
jeanrobertderome <jean-robert@deromelyons.ca>
2012-07-21 16:29:38 -0400

    Initial commit2

--- first_app/config/initializers/secret_token.rb
+++ first_app/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = 'c1ccfa38bba41c529f777eb33a11fec072bf3879c190ea0d7c5ea1738365af5bd1957401675cbd17c598fda265f2ec5a430450546118803ee8d0e904cae609cf'
+FirstApp::Application.config.secret_token = '1ef98ee861c8e9eb913224df9f2376e565ab3147780c03bc2f4579ea92809437636ed3e057c388dc9ffca9d999e02b124747ea5747a7311da41af9d81fd8c591'
